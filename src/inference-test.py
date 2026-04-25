c86eeeeef52734696beda492b84ac452094d9ccf
Jared Updike <jupdike@thebrain.com>
2026-04-25 11:20:13 -0700

    One more model to test

--- src/inference-test.py
+++ src/inference-test.py
@@ -13,10 +13,15 @@ from util import timestamped_filename, last_path_component, xmp_description_pack
 
 EDGE_OF_REALISM_MODEL_PATH = "models/edgeOfRealism_eorV20BakedVAE.safetensors"
 EPIC_REALISM_MODEL_PATH = "models/epicrealism_naturalSinRC1VAE.safetensors"
+EPIC_PHOTOG_MODEL_PATH = "models/epicphotogasm_zUniversal.safetensors"
+
+REALISTIC_VISION_MODEL_PATH = "models/todo/Realistic_Vision_V5_1.safetensors"
 
 # pick a model
 #model_path = EDGE_OF_REALISM_MODEL_PATH
-model_path = EPIC_REALISM_MODEL_PATH
+#model_path = EPIC_REALISM_MODEL_PATH
+#model_path = EPIC_PHOTOG_MODEL_PATH
+model_path = REALISTIC_VISION_MODEL_PATH
 # end pick a model
 
 model_name = last_path_component(model_path)
